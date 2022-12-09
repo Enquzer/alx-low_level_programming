7a4987d2d7f53279e6e254a38134cafa62a9ab80
Enquzer <enquzer@yahoo.co.uk>
2022-12-09 15:01:45 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints single digits base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
