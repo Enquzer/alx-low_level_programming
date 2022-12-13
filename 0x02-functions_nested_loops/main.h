a377d51f9f76bea1422ad7dc49e0e229a288a4a8
Enquzer <enquzer@yahoo.co.uk>
2022-12-13 15:28:32 -0800

    10times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,10 +3,11 @@
 /**
  *_putchar - writes the character c to stdout
  * @c: The character to print
- * print_alphabet will print something else
+ * print_alphabet - will print something else
  * Return: 0 success
  */
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
