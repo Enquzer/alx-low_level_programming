d42400e9455561e3485bf5c3d0d8417bc7288541
Enquzer <enquzer@yahoo.co.uk>
2022-12-13 14:55:34 -0800

    2nd qu

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- *  _putchar - writes the character c to stdout
- *  @c: The character to print
- *  RETURN 0
+ *_putchar - writes the character c to stdout
+ * @c: The character to print
+ * print_alphabet will print something else
+ * Return: 0 success
  */
-int _putchar(char);
+int _putchar(char c);
+void print_alphabet(void);
 
 #endif
