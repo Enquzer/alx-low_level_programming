38f9c719fb6c57b6d6045ec2c52878549552034a
Enquzer <enquzer@yahoo.co.uk>
2022-12-16 15:09:40 -0800

    all

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,87 +1,22 @@
-nclude<stdio.h>
+#include <stdio.h>
+/**
+ * main - combinations of two digit integers without duplicate
+ * 
+ * Return: 0 on success
+ */
 
-	
+int main(void)
+{
+	int i';
+	int i;
+	int j';
+	int j;
 
-	/**
-	 *
-	 * 	* main - a simple program that outputs unordered
-	 *
-	 * 		* combinations of two digit integers without duplicates
-	 *
-	 * 			*
-	 *
-	 * 				* Return: 0 on success
-	 *
-	 * 					*/
-
-	int main(void)
-
+	for (i' = 48; i < 58; i'++)
+	{
+		for (i = 48; i < 58; i++)
 		{
-
-				int ifirst;
-
-					int i;
-
-						int jfirst;
-
-							int j;
-
-								
-
-								for (ifirst = 48; ifirst < 58; ifirst++)
-
-										{
-
-												for (i = 48; i < 58; i++)
-
-														{
-
-																j = i + 1;
-
-																	jfirst = ifirst;
-
-																		for (; jfirst < 58; jfirst++)
-
-																				{
-
-																						for (; j < 58; j++)
-
-																								{
-
-																										putchar(ifirst);
-
-																											putchar(i);
-
-																												putchar(' ');
-
-																													putchar(jfirst);
-
-																														putchar(j);
-
-																															if (ifirst != 57 || jfirst != 57 || i != 56 || j != 57)
-
-																																	{
-
-																																			putchar(',');
-
-																																				putchar(' ');
-
-																																					}
-
-																																}
-
-																							j = 48;
-
-																								}
-
-																			}
-
-													}
-
-									putchar('\n');
-
-										return (0);
-
-											}
-
-
+			j = i + 1;
+			j' = i';
+			
+			for ( j' < 58; j'++)							{
