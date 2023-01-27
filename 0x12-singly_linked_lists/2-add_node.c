d4aac8c359dc710743ca01099f5532b794a25835
Enquzer <enquzer@gmail.com>
2023-01-27 03:30:48 -0800

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,7 +2,7 @@
 /**
  * add_node - adds a new node at the beginning of a list
  * @head: pointer to the head of the list
- * str: string to be added
+ * @str: string to be added
  * Return: returns the address to the new element or NUL
  */
 list_t *add_node(list_t **head, const char *str)
