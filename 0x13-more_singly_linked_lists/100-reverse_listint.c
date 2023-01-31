62db984b993b4f31cc62ca0654b766c5b7b76c26
Enquzer <enquzer@gmail.com>
2023-01-31 06:02:57 -0800

    more

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * reverse_listint - define function
+ * @head: describe argument
+ * Return: what does it return?
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
+}
