9f4dc82cc3c9224f52f0125b8f2e7e1e5467b4d8
Enquzer <enquzer@gmail.com>
2023-02-09 02:51:39 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, i = 0;
+	int file, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	file = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (file == -1)
 		return (-1);
-i++;
 
-if (text_content == NULL)
+	w =  write(file, text_content, len);
+
+if (text_content != NULL)
 {
-	close(file);
+	for (len = 0; text_content[len];)
+		len++;
 	return (-1);
-}
-else
-{
-	write(file, text_content, i);
 }
 
 close(file);
